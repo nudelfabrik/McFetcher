484e56d5c16104de89bdc09530dd2921421b5b3e
Benedikt Scholtes <bene.scholtes@googlemail.com>
2014-08-25 16:04:59 +0200

    check if newest version is also installed instead of only checked if downloaded.

--- McFetcher.sh
+++ McFetcher.sh
@@ -22,33 +22,42 @@ echo "Getting Mojang Feed..."
 wget --no-check-certificate https://mojang.com/feed/
 DPATH=$(grep -o -m 1 \"https://s3.amazonaws.com/Minecraft.Download/versions/.\*.jar\" index.html | sed 's/\"//g')
 FILE=$(echo $DPATH | grep -o minecraft_server.\*.jar)
+
+# Check if newest File already exists
 if [ -s $FILE ]
 then
     echo "No new Version available"
-    rm index.html
-    exit 0
 else
     echo "Downloading new Version"
     wget --no-check-certificate $DPATH
-    if [[ $1 == "--replace" ]]
-    then
-        echo "Stopping Server ..."
-        $MCSTOP
-        sleep 10
-        if [ -z "$(pgrep -u $MCID java)" ]
-        then
-            echo "Server is Down!"
-            echo "Replace minecraft_server.jar"
-            cp $FILE minecraft_server.jar
-        else
-            echo "Something is wrong!"
-            echo "Minecraft still running!"
-            exit 1
-        fi
-        echo "Starting Server"
-        $MCSTART
-    fi
+fi
+
+# Check if newest version is also the one used atm
+if [[ $(shasum $FILE | awk '{print $1}') -eq $(shasum minecraft_server.jar | awk '{print $1}') ]]
+then
+    echo "Newest Version already installed"
     rm index.html
     exit 0
-
 fi
+
+# update newest version
+if [[ $1 == "--replace" ]]
+then
+    echo "Stopping Server ..."
+    $MCSTOP
+    sleep 10
+    if [ -z "$(pgrep -u $MCID java)" ]
+    then
+        echo "Server is Down!"
+        echo "Replace minecraft_server.jar"
+        cp $FILE minecraft_server.jar
+    else
+        echo "Something is wrong!"
+        echo "Minecraft still running!"
+        exit 1
+    fi
+    echo "Starting Server"
+    $MCSTART
+fi
+rm index.html
+exit 0
